8a4b9fbe9c507985db8ae093bc2d8cbee0c9e9ca
Ravindra-1-8 <rr7937150@gmail.com>
2026-07-24 18:01:01 +0530

    slipms.py

---
+++ Strings slicing problems/slipms.py
@@ -0,0 +1,21 @@
+#Exercise on String slicing.
+#problem 1
+# Print the first 5 characters of a string.
+"""P1 ="PythonProgramming"
+print(P1[0:5])"""
+#output: Pytho
+
+# Problem 2
+#Print the last 4 characters of a string.
+"""print(P1[13:17])"""
+#output: "ming"
+
+#Problem 3
+#Print every second character.
+"""print(P1[::2])"""
+#output: "PtoPormig"
+
+#Problem 4: Reverse a string using slicing.
+"""P1 = "Python"
+print(P1[::-1])"""
+# output: "nohtyP"
