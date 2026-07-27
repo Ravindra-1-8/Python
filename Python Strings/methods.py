f22a4ebf1b78198ab00e5dc6c149a51c46abe3f7
Ravindra-1-8 <rr7937150@gmail.com>
2026-07-27 17:39:31 +0530

    methods.pyl

--- Python Strings/methods.py
+++ Python Strings/methods.py
@@ -33,3 +33,46 @@ print(a.capitalize())"""
 print(s.swapcase())
 a = "Hello WORLD"
 print(a.swapcase())"""
+
+#Problem 6
+"""s = "   Python   "
+print(s.strip())
+print(s.lstrip()) #remove leading spaces
+print(s.rstrip()) #remove trailing spaces"""
+
+#Problem 5: Replace the letters with another letter in a sentence.
+"""s = "I love Python"
+print(s.replace("Python","C++"))
+a = "Python is fun"
+print(a.replace(" ", "-"))"""
+
+#Problem 6: Find the letter and words in a sentence.
+"""s = "Hello World"
+print(s.find("W"))
+r = "The cat is sleeping"
+print(r.find("cat"))"""
+
+#Problem 7:Finds the position the letters.
+"""s = "Python"
+print(s.index("n"))
+print(s.index("thon"))"""
+
+#Problem 8: Finds the repetetion of the letters.
+"""s = "Banana is my favourite. But is Ripen"
+print(s.count("a"))
+print(s.count("is"))"""
+
+#Problem 9: Checks the condition
+"""s = "Python is fun.txt"
+print(s.startswith("Python"))
+print(s.startswith("p"))
+print(s.endswith("fun"))
+print(s.endswith(".txt"))"""
+
+#Problem 10:
+"""s = "Python Java C++"
+print(s.split())
+a = "apple,banana,mango"
+print(a.split(","))"""
+#Output: ['Python', 'Java', 'C++']
+#['apple', 'banana', 'mango']
