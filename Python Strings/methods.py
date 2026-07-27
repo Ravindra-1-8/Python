bee7712d77d4968880f9220ec06846b2344cdf2e
Ravindra-1-8 <rr7937150@gmail.com>
2026-07-27 17:33:04 +0530

    methods.py

--- Python Strings/methods.py
+++ Python Strings/methods.py
@@ -1,7 +1,11 @@
-a = " hello, world"
+"""a = " hello, world"
 
 print(a.upper())
 print(a.lower())
 print(a.strip())
 print('Abracadabra'.count('a'))
-print(a.replace('o','x'))
+print(a.replace('o','x'))"""
+
+#Problem 11:
+s = ["2026","07","27"]
+print(s)
