a4dc29bb7443b8e0e12919363213bdaef97fbb11
Ravindra-1-8 <rr7937150@gmail.com>
2026-07-27 17:37:36 +0530

    methods.py

--- Python Strings/methods.py
+++ Python Strings/methods.py
@@ -6,6 +6,33 @@ print(a.strip())
 print('Abracadabra'.count('a'))
 print(a.replace('o','x'))"""
 
+#Problem 1
+"""s = "hello world"
+print(s.upper())
+a = "PYTHON"
+print(a.lower())"""
+
+"""#Problem 2
+a = "python programming"
+print(a.upper())"""
+
+"""#Problem 3
+s = "welcome to python"
+print(s.title())
+s = "my name is ravindra raj"
+print(s.title())"""
+
+#Problem 4
+"""s = "hello world"
+print(s.capitalize())
+a = "python IS fun"
+print(a.capitalize())"""
+
+#Problem 5
+"""s = "PyThOn"
+print(s.swapcase())
+a = "Hello WORLD"
+print(a.swapcase())"""
 #Problem 11:
 s = ["2026","07","27"]
-print(s)
+print()
