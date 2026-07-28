08aad2fe05b2dba775a3f3cd6f7f245208a8ad3c
Ravindra-1-8 <rr7937150@gmail.com>
2026-07-28 17:07:41 +0530

    stringformatting.py

--- Python Strings/string formatting.py
+++ Python Strings/string formatting.py
@@ -1,9 +1,7 @@
-"""print("My name is %s %s and my age is %d" % ("John", "Doe", 45)) 
+print("My name is %s %s and my age is %d" % ("John", "Doe", 45)) 
 
 name = "Alice"
 age = 88
 print(f"My name is {name} and I am {age} years old")
-
-"""
 
 print("hello\rs world")  #escape sequence for including special characters.
