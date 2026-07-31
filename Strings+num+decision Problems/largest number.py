ea87462801ca4b45585c99d1276d75c43a4d6eba
Ravindra-1-8 <rr7937150@gmail.com>
2026-07-31 17:36:56 +0530

    largestnumber.py

---
+++ Strings+num+decision Problems/largest number.py
@@ -0,0 +1,5 @@
+# wap takes 3 nums as input and finds largest among them using decision making statements.
+# input: Enter three numbers: 15,8,21
+# output: The largest number is 21.
+
+a = input() 
--- Strings+num+decision Problems/vowel.py
+++ Strings+num+decision Problems/vowel.py
@@ -7,4 +7,4 @@ i = s2.count("i")
 o = s2.count("o")
 u = s2.count("u")
 
-print(f"Number of vowels:{a+e+i+o+u}")
+print(f"Number of vowels:{a+e+i+o+u}") 
