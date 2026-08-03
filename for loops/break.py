75d0155a2f0a0935e57993249da446021462ccd6
Ravindra-1-8 <rr7937150@gmail.com>
2026-08-03 17:24:09 +0530

    break.py

---
+++ for loops/break.py
@@ -0,0 +1,8 @@
+candies = 5
+
+for i in range(candies):
+    print("Giving candies to friend.")
+
+    if candies - 1 == 5:
+        print("Only 5 chocolates are left. Stop the distribution.")
+        break
