3b6a3e35db0fde008a81bd4a5084d4e58a477ba6
Ravindra-1-8 <rr7937150@gmail.com>
2026-08-05 16:06:27 +0530

    even.py

---
+++ for loops/even.py
@@ -0,0 +1,7 @@
+n = int(input("Give value of N: "))
+
+i = 1
+while i <= n:
+    if i % 2 == 0:
+        print(i)
+    i += 1
---
+++ for loops/factorial.py
@@ -0,0 +1,7 @@
+n = int(input("Give value of N: "))
+
+fact = 1
+while n > 0:
+    fact = fact * n
+    n += 1      #(n,n-1,n-2,......1)
+print(fact)
