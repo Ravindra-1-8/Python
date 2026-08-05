d19bdc69ef9bc8c7b079e15d9f16c29de949878f
Ravindra-1-8 <rr7937150@gmail.com>
2026-08-05 16:49:48 +0530

    multitable.py

---
+++ for loops/multi table.py
@@ -0,0 +1,10 @@
+n = int(input("Give value of N: "))
+
+for i in range(1,11):
+    print(f"{n} * {i} = {n * i}")
+
+""" while loop
+i = 1
+while i <= 10:
+    print(f"{n} * {i} = {n * i}")
+    i += 1"""
