7b21f809b2f8b8ca80a897fec6c4d9afd4fda292
Ravindra-1-8 <rr7937150@gmail.com>
2026-08-06 12:25:21 +0530

    first.py

--- functions/first.py
+++ functions/first.py
@@ -1,3 +1,6 @@
 def sum(a,b):  #function definition in the parantheses are Parameters.
     print(a+b)
-sum(5,9)  #function call in the braces are arguments.
+    
+#sum(5,9)  #function call in the braces are arguments.
+result = sum(3,5)
+print(result)
