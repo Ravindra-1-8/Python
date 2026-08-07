abd293c91ffa60e77124883e543c112c35ff4b75
Ravindra-1-8 <rr7937150@gmail.com>
2026-08-07 13:34:03 +0530

    areaofcicel.py

--- Problem on Function/areaof circel.py
+++ Problem on Function/areaof circel.py
@@ -1,13 +1,18 @@
-"""def area(r = 2):
+def area(r = 2):
+
     cirleArea = 3.14*(r*2)
     return cirleArea
 
 # radius = 1
 # a = area(radius)
 a = area()
-print(a)"""
+print(a)
+
+""" Method 2
 
 def area(r = 2):
+
     return 3.14*(r**2)
+
 a = area()
-print(a)
+print(a)"""
