312c4b50cf7e07039bec7ad2d92c380bf6eb3e03
Ravindra-1-8 <rr7937150@gmail.com>
2026-08-07 13:33:05 +0530

    areaofcircle.py

---
+++ Problem on Function/areaof circel.py
@@ -0,0 +1,13 @@
+"""def area(r = 2):
+    cirleArea = 3.14*(r*2)
+    return cirleArea
+
+# radius = 1
+# a = area(radius)
+a = area()
+print(a)"""
+
+def area(r = 2):
+    return 3.14*(r**2)
+a = area()
+print(a)
---
+++ Problem on Function/sumof2num.py
@@ -0,0 +1,13 @@
+"""a = int(input())
+b = int(input())
+
+def add2nums(a,b):
+    print(a+b)
+add2nums(a,b)
+"""
+# Second method
+def add2nums(c,d):
+    print(c+d)
+a = int(input())
+b = int(input())
+add2nums(a,b)
