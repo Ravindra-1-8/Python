b394948969422cb2f6f9f3fc1b80a464f4e3b38b
Ravindra-1-8 <rr7937150@gmail.com>
2026-08-07 11:55:27 +0530

    keyword.py

---
+++ functions/key word.py
@@ -0,0 +1,11 @@
+def information(name, age):
+    print("Name:", name)
+    print("Age", age)
+
+information(age=85, name="Garry")
+
+def loved_flower(nam, sovergnity):
+    print("Nam:", nam)
+    print("Sovergnity:", sovergnity)
+
+loved_flower(nam="Tulips", sovergnity="God")
