efc783c3d21719fa5e2bb59b2263144d1e99a2bd
Ravindra-1-8 <rr7937150@gmail.com>
2026-08-09 17:55:37 +0530

    setoperations.py

---
+++ Sets/set add.py
@@ -0,0 +1,5 @@
+my_set = {3,4,6,6}
+my_set.add(7)
+my_set.remove(3)
+my_set.discard(4)
+print(my_set)
---
+++ Sets/set operations.py
@@ -0,0 +1,19 @@
+set1 = {1,2,3,4,5}
+set2 = {6,7,8,9,10,5}
+
+fset1 = set1.union(set2)
+fset2 = set1.intersection(set2)
+
+print(fset1)
+print(fset2) 
+
+s1 = {1,2,3,4,5}
+s2 = {1,2,3,4,5,18}
+
+res = s2.difference(s1)
+print(res)
+
+r1 = {1,3,5,7}
+r2 = {1,2,3,4,5,6,7}
+res2 = r1.symmetric_difference(r2)
+print(res2)
