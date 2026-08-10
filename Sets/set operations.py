7ee71f7061a02f44f65a6422ad9352c2dfd359cb
Ravindra-1-8 <rr7937150@gmail.com>
2026-08-10 11:04:09 +0530

    setoperations.py

--- Sets/set operations.py
+++ Sets/set operations.py
@@ -17,3 +17,10 @@ r1 = {1,3,5,7}
 r2 = {1,2,3,4,5,6,7}
 res2 = r1.symmetric_difference(r2)
 print(res2)
+
+f2 = {2,4,5,2,5,7}
+f1 = {1,1,3,4,5,6}
+union = f1.union(f2)
+intersection = f1.intersection(f2) # return the common elements in both the sets.
+print(union)
+print(intersection)
