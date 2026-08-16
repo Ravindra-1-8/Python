92f6e018b3db25204cabb358ee8c016eaaff8ea0
Ravindra-1-8 <rr7937150@gmail.com>
2026-08-16 17:13:56 +0530

    deffunctions.py

---
+++ Basics/def functions.py
@@ -0,0 +1,9 @@
+"""def add_two_numbers(num_one,num_two):
+    total = num_one + num_two
+    print(total)
+add_two_numbers(3,8)"""
+
+# using return
+def sum_two_numbers(sum_one,sum_two):
+    return sum_one + sum_two
+print(sum_two_numbers(3,4))
