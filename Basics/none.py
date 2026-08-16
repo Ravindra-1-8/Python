be893a0b5deb870bc59b7477c907944c01133a54
Ravindra-1-8 <rr7937150@gmail.com>
2026-08-16 17:24:42 +0530

    none.py

---
+++ Basics/none.py
@@ -0,0 +1,13 @@
+def square_a_number(number):
+    square = number * number
+    return
+print(square_a_number(4))
+
+# Second Example
+def add_two_numbers(num_one,num_two):
+    result = num_one + num_two
+print(add_two_numbers(5,4))
+
+#Assinging a function call to a variable and printing the variable will also return none.
+sum_without_return = add_two_numbers(8,6)
+print(sum_without_return)
