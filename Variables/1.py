399303059913e68fd1b50d9827ad1aba1a489a4e
Ravindra-1-8 <rr7937150@gmail.com>
2026-08-16 16:45:54 +0530

    1.py

---
+++ Variables/1.py
@@ -0,0 +1,10 @@
+my_first_variable = 1
+my_first_variable = 2
+
+print(type(my_first_variable))
+
+my_second_variable = "Now, I'm a string."
+print(type(my_second_variable))
+
+my_second_variable = 'You can call me string "str" '
+print(my_second_variable)
