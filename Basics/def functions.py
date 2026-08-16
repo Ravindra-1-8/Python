bb4abb33b84201545609c3a712b77afa3e208edf
Ravindra-1-8 <rr7937150@gmail.com>
2026-08-16 17:17:04 +0530

    deffunctions.py

--- Basics/def functions.py
+++ Basics/def functions.py
@@ -7,3 +7,7 @@ add_two_numbers(3,8)"""
 def sum_two_numbers(sum_one,sum_two):
     return sum_one + sum_two
 print(sum_two_numbers(3,4))
+
+# Anotherr efficient method using return.
+sum_with_return = sum_two_numbers(5, 9)
+print(sum_with_return)
