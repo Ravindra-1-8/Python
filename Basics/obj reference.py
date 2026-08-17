e993562635599d3c245ec25f9fc23697dcb8d3fa
Ravindra-1-8 <rr7937150@gmail.com>
2026-08-17 19:58:10 +0530

    practicalexample.py

---
+++ Basics/obj reference.py
@@ -0,0 +1,14 @@
+# Concept of Object reference.
+x = 5
+y = x
+x = 'Raj,'
+y = 'Mystic force'
+
+print(x,y)
+
+# Output: Raj, Mystic force. The value assigned to x = 5 went to garbage.
+
+# Deleting a variabel.
+k = 100
+del k
+print(k)
---
+++ Basics/practical example.py
@@ -0,0 +1,11 @@
+# Swapping two variables.
+a, b = 5, 10
+a, b = b, a
+print(a,b)
+
+# Counting characters in a string.
+# Assigning result of multiple operationson a string to variable in one line.
+
+word = 'python'
+length = len(word)
+print(f"Length of the word: {length}")
