12bcb4a21be8165afb220a7ae855aedbcce804af
Ravindra-1-8 <rr7937150@gmail.com>
2026-08-31 19:08:42 +0530

    Config.py

--- OOP's Concept/Class and Objects.py
+++ OOP's Concept/Class and Objects.py
@@ -6,7 +6,7 @@ a = '0'
 print(type(a)) #output: <class 'str'> str is inbuilt class.
 
 x = 9
-print(type(x))
+print(type(x)) #output: <class 'ínt'> int is inbuilt class.
 
 com1 = Computer()
 print(type(com1)) #output: <class '__main__.Computer'> while Computer is a user-defined class.
---
+++ OOP's Concept/Config.py
@@ -0,0 +1,6 @@
+class Computer:
+    def config(self):
+        print("i5, 16gb, 1TB.")
+
+com1 = Computer()
+Computer.config(com1)  #output: i5, 16gb, 1TB.
