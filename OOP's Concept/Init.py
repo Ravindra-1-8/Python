c7e9ce30f25b8023ab584a703eb205dbc178e175
Ravindra-1-8 <rr7937150@gmail.com>
2026-09-03 19:41:32 +0530

    Init.py

---
+++ OOP's Concept/Init.py
@@ -0,0 +1,9 @@
+class Computer:
+    def config(self):
+     print("i5, 16GB, 1TB")
+    
+com1 = Computer()
+com2 = Computer()
+
+com1.config()
+com2.config() # We can call the config method using the object of the class.
