a5bc78f9d908b7bb15279b00db7c0cbeb729eedf
Ravindra-1-8 <rr7937150@gmail.com>
2026-09-03 19:39:40 +0530

    Init.py

--- OOP's Concept/Config.py
+++ OOP's Concept/Config.py
@@ -5,10 +5,10 @@ class Computer:
 com1 = Computer()
 com2 = Computer()
 
-Computer.config(com1)  #output: i5, 16gb, 1TB.
+Computer.config(com1)  #output: i5, 16gb, 1TB. We can call config using the "object".
 Computer.config(com2)  #output: i5, 16gb, 1TB.
 
-com1.config()  #output: i5, 16gb, 1TB.
+com1.config()  #output: i5, 16gb, 1TB. We can call with help of "class" by passing the "object" as an argument.
 com2.config()  #output: i5, 16gb, 1TB.
 
 a = 5
