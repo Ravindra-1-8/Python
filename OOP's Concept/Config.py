c4b2ffe1b829961dcaa8e1b2d1dc1c6ba450ddf5
Ravindra-1-8 <rr7937150@gmail.com>
2026-09-03 18:19:50 +0530

    Config.py

--- OOP's Concept/Config.py
+++ OOP's Concept/Config.py
@@ -3,4 +3,10 @@ class Computer:
         print("i5, 16gb, 1TB.")
 
 com1 = Computer()
+com2 = Computer()
+
 Computer.config(com1)  #output: i5, 16gb, 1TB.
+Computer.config(com2)  #output: i5, 16gb, 1TB.
+
+com1.config()  #output: i5, 16gb, 1TB.
+com2.config()  #output: i5, 16gb, 1TB.
