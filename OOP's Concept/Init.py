bf8055dec0f851070dc8ba20c907277c4ed82d20
Ravindra-1-8 <rr7937150@gmail.com>
2026-09-04 16:01:58 +0530

    Init.py

--- OOP's Concept/Init.py
+++ OOP's Concept/Init.py
@@ -1,9 +1,10 @@
 class Computer:
-    def __init__(self):
+    def __init__(self,cpu,ram):
         print("This is a Computer class constructor.")
         
-com1 = Computer() # This is Object creation.
-com2 = Computer()
+        
+com1 = Computer("i5, 16") # com1 is an Object for class.
+com2 = Computer("Ryzen, 32") # com2 is an Object for class.
 
 com1.__init__()
 com2.__init__()
